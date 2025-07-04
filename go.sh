e0b0e7d931c9d11d29c9d4bac77bcfd87d5e02c2
Tomáš Mráz <tomas.mraz@zona64.com>
2025-07-04 09:10:15 +0200

    AAA

---
+++ go.sh
@@ -0,0 +1,8 @@
+/home/tomas/Android/Sdk/cmake/4.0.2/bin/cmake \
+  -H/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/src/main/cpp \
+  -B/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/.cxx/Debug/3v82m1s2/arm64-v8a \
+  -DCMAKE_ANDROID_NDK=/home/tomas/Android/Sdk/ndk/26.3.11579264 \
+  -DCMAKE_TOOLCHAIN_FILE=/home/tomas/Android/Sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake \
+  -DCMAKE_MAKE_PROGRAM=/home/tomas/Android/Sdk/cmake/4.0.2/bin/ninja \
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/build/intermediates/cxx/Debug/3v82m1s2/obj/arm64-v8a \
+  -G Ninja
---
+++ go2.sh
@@ -0,0 +1,15 @@
+/home/tomas/Android/Sdk/cmake/4.0.2/bin/cmake \
+  -H/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/src/main/cpp \
+  -B/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/.cxx/Debug/3v82m1s2/arm64-v8a \
+  -DCMAKE_SYSTEM_NAME=Android \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DCMAKE_SYSTEM_VERSION=24 \
+  -DANDROID_PLATFORM=android-24 \
+  -DANDROID_ABI=arm64-v8a \
+  -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
+  -DCMAKE_ANDROID_NDK=/home/tomas/Android/Sdk/ndk/26.3.11579264 \
+  -DCMAKE_TOOLCHAIN_FILE=/home/tomas/Android/Sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake \
+  -DCMAKE_MAKE_PROGRAM=/home/tomas/Android/Sdk/cmake/4.0.2/bin/ninja \
+  -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/build/intermediates/cxx/Debug/3v82m1s2/obj/arm64-v8a \
+  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=/home/tomas/git-osobni-github/studio-gradle-cmake-go/aplikace2/app/build/intermediates/cxx/Debug/3v82m1s2/obj/arm64-v8a \
+  -G Ninja
